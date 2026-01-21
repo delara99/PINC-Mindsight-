0ba7092ba8a8aa683715be76d827056337613f2a
Delara <delara@example.com>
2026-01-21 14:31:10 -0300

    fix(module): import PdfService into TalkingToModule

--- backend/src/talking-to/talking-to.module.ts
+++ backend/src/talking-to/talking-to.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TalkingToController } from './talking-to.controller';
 import { TalkingToService } from './talking-to.service';
 import { PrismaModule } from '../prisma/prisma.module';
-
 import { ScoreCalculationService } from '../reports/score-calculation.service';
+import { PdfService } from '../reports/pdf.service';
 
 @Module({
     imports: [PrismaModule],
     controllers: [TalkingToController],
-    providers: [TalkingToService, ScoreCalculationService],
+    providers: [TalkingToService, ScoreCalculationService, PdfService],
     exports: [TalkingToService]
 })
 export class TalkingToModule { }
