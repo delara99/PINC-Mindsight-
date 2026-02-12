96b8e2b0e4f797b43771df08031ac11491dbed3b
Delara <delara@example.com>
2026-02-12 16:16:27 -0300

    fix(frontend): add API proxy rewrite to solve 404 on Google Auth callback

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/v1/:path*',
+                destination: 'https://pinc-mindsight-production.up.railway.app/api/v1/:path*',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
